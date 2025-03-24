8cb9f22749cd185548b0acb57ad3a99ec202d5d5
prajwalrs123 <115072798+prajwalrs123@users.noreply.github.com>
2025-03-24 21:18:41 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,5 @@
-// Simple Java Hello World Program
-class HelloWorld
-{
-    public static void main(String[] args)
-    {
-        System.out.println(“Hello, World”);
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
     }
 }
